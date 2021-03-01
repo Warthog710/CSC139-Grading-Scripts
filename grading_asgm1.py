0a32491eef65ed2d1acba4f763332d6921de0ddc
Warthog710 <computergeek710@gmail.com>
2021-03-01 12:07:33 -0800

    Implemented compile_submissions
    Compiler errors are saved in .txt with names corresponding to the .c file that produced the error.

--- grading_asgm1.py
+++ grading_asgm1.py
@@ -70,6 +70,40 @@ def rename_submissions():
 def compile_submissions():
     print('Compiling submissions...')
 
+    #Read through all directories and compile the students files
+    for entry in os.listdir():
+        #Ignore .vscode/.git folder and only look at directories
+        if entry.startswith('.') or not os.path.isdir(entry):
+            continue
+
+        #Enter the directory
+        os.chdir('./' + entry)
+
+        #Loop through files in directory and compile saving errors
+        for file in os.listdir():            
+            #Compile the producer, ignoring warnings, saving any errors (if any)
+            if 'producer.c' in file:
+                error_file = open('./producer_compiler_errors.txt', 'w')
+                subprocess.run(['gcc', './producer.c', '-lrt', '-w', '-o', 'producer'], stderr=error_file)
+                error_file.close()
+
+                #If the error file is size == 0, remove it as no errors occured
+                if os.stat('./producer_compiler_errors.txt').st_size == 0:
+                    os.remove('./producer_compiler_errors.txt')
+
+            #Compile the consumer, ignoring warnings, saving any errors (if any)
+            elif 'consumer.c' in file:
+                error_file = open('./consumer_compiler_errors.txt', 'w')
+                subprocess.run(['gcc', './consumer.c', '-lrt', '-w', '-o', 'consumer'], stderr=error_file)
+                error_file.close()
+
+                #If the error file is size == 0, remove it as no errors occured
+                if os.stat('./consumer_compiler_errors.txt').st_size == 0:
+                    os.remove('./consumer_compiler_errors.txt')
+
+        #Go back a directory
+        os.chdir('..')
+
 def run_test_cases(test_cases):
     print('Running test cases...')
 
@@ -78,3 +112,4 @@ test_cases = read_test_cases()
 print(test_cases)
 sort_submissions()
 rename_submissions()
+compile_submissions()
